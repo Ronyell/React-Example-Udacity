025299ca9eebd81b163c154f400613eedee58ca5
Ronyell Henrique <ronyellhenrique@gmail.com>
2017-12-20 23:45:42 -0200

    Updating dependencies, app and readme

--- App.js
+++ App.js
@@ -1,56 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import firebase from 'firebase';
-import { Header, Spinner, Button, Card, CardSection } from './src/components/common';
-import LoginForm from './src/components/LoginForm';
+import Application from './src/Application';
 
 export default class App extends React.Component {
-  state = { loggedIn: null };
-
-  componentWillMount() {
-    firebase.initializeApp({
-        apiKey: "AIzaSyB8Nu_UodIuXKdX7F26RZpPOb7y3ar2-Ro",
-        authDomain: "testreactnative-f75bd.firebaseapp.com",
-        databaseURL: "https://testreactnative-f75bd.firebaseio.com",
-        projectId: "testreactnative-f75bd",
-        storageBucket: "testreactnative-f75bd.appspot.com",
-        messagingSenderId: "163873459102"
-    });
-
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ loggedIn: true });
-      } else {
-        this.setState({ loggedIn: false });
-      }
-    });
-  }
-
-  renderCotent(){
-    switch (this.state.loggedIn) {
-      case true:
-        return (
-          <Card>
-            <CardSection>
-              <Button onPress={() => firebase.auth().signOut()}>
-                Logout
-              </Button>
-            </CardSection>
-        </Card>
-        );
-      case false:
-        return <LoginForm />;
-      default:
-        return <Spinner size="large" />
-    }
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Header headerText={'Authentication'} />
-        {this.renderCotent()}
-      </View>
+      <Application />
     );
   }
 }
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-sudo npm install -g react-native-cli
-sudo npm install -g expo
-sudo exp init <Nome do Projeto>
-sudo exp start
+# Comandos
 
-sudo npm install --save axios
-sudo npm install --save firebase
+* sudo npm install -g react-native-cli
+* sudo npm install -g exp
+* sudo exp init <Nome do Projeto>
+* sudo exp start
+
+# Comandos
+
+* sudo npm install --save redux react-redux
+* sudo npm install --save axios
+* sudo npm install --save firebase
